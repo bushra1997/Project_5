24300504b8fb3ae30cb048bb2189c3fd356529ec
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-06-24 12:39:44 +0300

    server connect

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2');
+require('dotenv').config()
 
+//console.log(process.env.DB_HOST,process.env.DB_USER, process.env.DB_PASS,process.env.DB_NAME);
 const connection = mysql.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
