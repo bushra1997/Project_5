dfec9d3be180bfa11178df70760412c393e0865c
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-06-24 20:49:44 +0300

    File strucuers Alaa changes 2

--- Project_5/frontend/src/components/navigation/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Navigation = () => {
-	return <div className="App">Navigation component</div>;
-};
-
-export default Navigation;
--- backend/db/servisesPlaces.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE servisesPlaces(
-id INT AUTO_INCREMENT NOT NULL,
-costServises VARCHAR(255),
-place_id INT,
-FOREIGN KEY (place_id ) REFERENCES place(id),
-extraServices_id INT,
-FOREIGN KEY (extraServices_id ) REFERENCES extraServices(id),
-PRIMARY KEY (id)
-);
