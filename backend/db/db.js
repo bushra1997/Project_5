d0a1d32993bb07e3ec7be6cb9da9329d78c066ff
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-06-24 15:42:15 +0300

    create tables and connect databases

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config()
 
-//console.log(process.env.DB_HOST,process.env.DB_USER, process.env.DB_PASS,process.env.DB_NAME);
 const connection = mysql.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
--- backend/db/roles.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE roles (
-    id INT AUTO_INCREMENT NOT NULL,
-    role VARCHAR(255),
-    is_deleted TINYINT DEFAULT 0,
-    -- primary key must be unique for each record and there must be only one primary key
-    PRIMARY KEY (id)
-);
