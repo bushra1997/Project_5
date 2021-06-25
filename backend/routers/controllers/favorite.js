37c8867ff03bbf0395fcb1f6388ded8a6f73dc02
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-06-25 20:09:08 +0300

    fovarite added

---
+++ backend/routers/controllers/favorite.js
@@ -0,0 +1,18 @@
+const db = require("../../db/db");
+
+const CreateNewFavorate=(req,res)=>{
+
+    const {place_id,user_id} = req.body;
+    const query = `INSERT INTO favorite (place_id,user_id) VALUES (?,?)`;
+    const data = [user_id, place_id];
+    db.query(query, data, (err, result) => {
+        if (err) throw err;
+    console.log('RESULT: ', result);
+    res.json(result)
+      });
+
+}
+
+module.exports = {
+    CreateNewFavorate
+  };
