3775093435baa4fbba48691ee5c128198e9b9708
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-06-26 23:15:30 +0300

    getAllPlaces,   updatePlaceById,   deletePlaceById,   getPlaceById

--- backend/routers/controllers/place.js
+++ backend/routers/controllers/place.js
@@ -1,4 +1,5 @@
 const connection = require("../../db/db");
+
 const CreateNewPlace = (req, res) => {
   const { name, description, img, capacity, city, address, availability } =
     req.body;
@@ -10,19 +11,47 @@ const CreateNewPlace = (req, res) => {
   });
 };
 
-const getAllPlaces=(req,res)=>{
-    const { name, description, img, capacity, city, address, availability } =
-    req.body;
-  const query = `INSERT INTO place(name,description,img,capacity,city,address,availability)VALUES(?,?,?,?,?,?,?)`;
-  const data = [name, description, img, capacity, city, address, availability];
-  connection.query(query, data, (err, result) => {
+const getAllPlaces = (req, res) => {
+  const query = `SELECT * FROM place`;
+  connection.query(query, (err, result) => {
     if (err) throw err;
     res.json(result);
   });
-}
+};
 
+const updatePlaceById = (req, res) => {
+  const id = req.params.id;
+  `UPDATE place SET name=?,description=?,img=?,capacity=?,city=?,address=?,availability=?, WHERE id =? `;
+  connection.query(query, id, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+};
+
+const deletePlaceById = (req, res) => {
+  const id = req.params.id;
+  const query = `DELETE FROM place 
+    WHERE id =? `;
+  db.query(query, id, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+};
+
+const getPlaceById = (req, res) => {
+    const id = req.params.id;
+    const query = `SELECT * FROM place 
+      WHERE id =? `;
+    db.query(query, id, (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  };
 
 module.exports = {
   CreateNewPlace,
+  getAllPlaces,
+  updatePlaceById,
+  deletePlaceById,
+  getPlaceById
 };
-
--- backend/routers/routes/place.js
+++ backend/routers/routes/place.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const placeRouter = express.Router();
-const { CreateNewPlace } = require("./../controllers/place");
+const { CreateNewPlace,getAllPlaces,updatePlaceById,deletePlaceById,getPlaceById } = require("./../controllers/place");
 placeRouter.post("/", CreateNewPlace);
+placeRouter.get("/place", getAllPlaces);
+placeRouter.put("/places/:id",updatePlaceById);
+placeRouter.delete("/places/:id",deletePlaceById);
+placeRouter.get("/places/:id",getPlaceById);
 module.exports = placeRouter;
