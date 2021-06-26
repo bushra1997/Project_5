57366b3f0ea9cc71d7fbd219918e0f315ee57ee7
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-06-26 17:17:37 +0300

    form validaion when sign up

--- backend/routers/controllers/user.js
+++ backend/routers/controllers/user.js
@@ -1,15 +1,23 @@
 const db = require("../../db/db");
+const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
 
 const CreateNewUser = (req, res) => {
-  const { firstName, lastName, age, country, email, password, role_id } =
-    req.body;
-  const query = `INSERT INTO users (firstName,lastName,age,country,email,password,role_id) VALUES (?,?,?,?,?,?,?)`;
-  const data = [firstName, lastName, age, country, email, password, role_id];
-  db.query(query, data, (err, result) => {
-    if (err) throw err;
-    console.log("RESULT: ", result);
-    res.json(result);
-  });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const alert = errors.array();
+    res.json(alert);
+  } else {
+    const { firstName, lastName, age, country, email, password, role_id } =
+      req.body;
+
+    const query = `INSERT INTO users (firstName,lastName,age,country,email,password,role_id) VALUES (?,?,?,?,?,?,?)`;
+    const data = [firstName, lastName, age, country, email, password, role_id];
+    db.query(query, data, (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  }
 };
 
-module.exports={CreateNewUser};
+module.exports = { CreateNewUser };
