1f51e362def701bfb5cb637e7b50ad43c89c7e47
Noof <noof.mohammed998@gmail.com>
2021-06-26 15:57:02 +0300

    createRating

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,14 @@ const roleRouter = require("./routers/routes/role");
 const sliderRouter = require("./routers/routes/slider");
 const favoriteRouter = require("./routers/routes/favorite");
 const userRouter = require("./routers/routes/user");
+const ratingRouter = require("./routers/routes/rating");
 
 // router middleware
 app.use("/role", roleRouter);
 app.use("/slider", sliderRouter);
 app.use("/favorite", favoriteRouter);
 app.use("/user", userRouter);
+app.use("/rating", ratingRouter);
 
 //built-in middlewares
 app.use(express.json());
