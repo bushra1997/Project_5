ade2241980b754ca09fb81aeb146836bd2f830f0
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-06-27 12:56:40 +0300

    write useState

---
+++ frontend/src/components/register.js
@@ -0,0 +1,20 @@
+import {React,useState} from "react";
+import axios from 'axios';
+import React from 'react';
+
+export default function Register() {
+
+const [firstName, setFirstName] = useState('')
+const [lastName, setLastName] = useState('')
+const [age, setAge] = useState(0)
+const [country, setCountry] = useState('')
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
