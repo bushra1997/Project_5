8b84298393d1710e7e5325595e8d2993b81bc5b9
Noof <noof.mohammed998@gmail.com>
2021-06-28 09:19:02 +0300

    widingComponent

---
+++ frontend/src/components/widings/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { Route, Link, useHistory } from "react-router-dom";
+const Weddings = () => {
+    return <h4>Weddings</h4>
+//    useHistory.push("/Logo");
+ };
+ export default Weddings
