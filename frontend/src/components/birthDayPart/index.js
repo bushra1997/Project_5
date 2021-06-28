f8e17fff7ef1a270d28c9627e7842684c02df282
Noof <noof.mohammed998@gmail.com>
2021-06-28 09:21:01 +0300

    birthDayPart

---
+++ frontend/src/components/birthDayPart/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { Route, Link, useHistory } from "react-router-dom";
+const BirthDayParty = () => {
+    return <h4>BirthDayParty</h4>
+//    useHistory.push("/Logo");
+ };
+ export default BirthDayParty
