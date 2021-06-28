61ea5f583dfec00bc680f7df2afb5eb212ab1a7f
Noof <noof.mohammed998@gmail.com>
2021-06-28 09:25:06 +0300

    dinnerParty

---
+++ frontend/src/components/dinnerParty/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { Route, Link, useHistory } from "react-router-dom";
+const DinnerParty = () => {
+    return <h4>DinnerParty</h4>
+//    useHistory.push("/Logo");
+ };
+ export default DinnerParty
