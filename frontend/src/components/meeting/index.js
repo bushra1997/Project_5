f1cea90b1cbd02c5c8c2c57906fcb5dfbee9a329
Noof <noof.mohammed998@gmail.com>
2021-06-28 09:27:20 +0300

    meetingComponent

---
+++ frontend/src/components/meeting/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { Route, Link, useHistory } from "react-router-dom";
+const Meeding = () => {
+    return <h4>Meeding</h4>
+//    useHistory.push("/Logo");
+ };
+ export default Meeding
