0bfa95a310c47cdaa37a86118a6ceb34e7896704
Noof <noof.mohammed998@gmail.com>
2021-06-28 09:26:19 +0300

    funeralsComponent

---
+++ frontend/src/components/Funerals/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { Route, Link, useHistory } from "react-router-dom";
+const Funerals = () => {
+    return <h4>Funerals</h4>
+//    useHistory.push("/Logo");
+ };
+ export default Funerals
