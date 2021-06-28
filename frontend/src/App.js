8747dff20c39637483354dfe939dc0ee123f1fc3
Noof <noof.mohammed998@gmail.com>
2021-06-28 09:36:15 +0300

    change app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Navigation from "./components/navigation/index";
+import Weddings from "./components/widings"
+import BirthDayParty from "./components/birthDayPart/index"
+import Funerals from "./components/Funerals/index"
+import DinnerParty from "./components/birthDayPart"
+import Meeting from "./components/meeting"
 
 const App = () => {
 	// return <div className="App">App component</div>;
 	return(
 		<>
-		{<Navigation/>}
+		<Navigation/>
+		<Route path="/weddings" component={Weddings}/>
+		<Route path="/birthDayParty" component={BirthDayParty}/>
+		<Route path="/funerals" component={Funerals}/>
+		<Route path="/dinnerParty" component={DinnerParty}/>
+		<Route path="/meeting" component={Meeting}/>
+
 		</>
 	)
 };
