01bd188165df6eef39f6a81c426a42f8ec0d9165
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-06-29 12:23:22 +0300

    Nav bar modified and weddings place works on

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import { Route, Switch } from "react-router-dom";
-import Navigation from "./components/navigation/index";
-import Weddings from "./components/widings";
+import Navigation from "./components/navigation/Navigation";
+import Weddings from "./components/weddings/index";
 import BirthDayParty from "./components/birthDayPart/index";
 import Funerals from "./components/Funerals/index";
 import DinnerParty from "./components/birthDayPart";
 import Meeting from "./components/meeting";
 import Login from "./components/auth/login/Login";
-import Register from "./components/Register";
+import Register from "./components/register";
 
 const App = () => {
   return (
