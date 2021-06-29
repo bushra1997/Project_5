b5bb1a9b9aaf1f2f40eb3a1851c898696f7459dc
Noof <noof.mohammed998@gmail.com>
2021-06-29 20:12:41 +0300

    fix footer position

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   return (
     <>
       <Navigation />
-      <Footer />
+
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
@@ -28,6 +28,7 @@ const App = () => {
         <Route path="/dinnerParty" component={DinnerParty} />
         <Route path="/meeting" component={Meeting} />
       </Switch>
+      <Footer />
     </>
   );
 };
