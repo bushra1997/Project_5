60dc60c7e5501f7cc9dd15f515b02ac8ef1c5107
Noof <noof.mohammed998@gmail.com>
2021-06-29 10:51:44 +0300

    add css link

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./reducers";
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 ReactDOM.render(
   <Router>
