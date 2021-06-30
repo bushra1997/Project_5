b9dbf28586a2c0d5d51c8fd82a982f7f4a20a1a3
Noof <noof.mohammed998@gmail.com>
2021-06-30 13:22:35 +0300

    getPlaceById function

--- frontend/src/components/Funerals/index.js
+++ frontend/src/components/Funerals/index.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useHistory,useParams } from "react-router-dom";
 import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./funerals.css"
 
+
+
 const Funerals = () => {
+  const history = useHistory();
   const [places, setPlaces] = useState([]);
+  const { id } = useParams();
+
 
   const ShowCard = ({ place }) => {
     return (
@@ -45,9 +51,12 @@ const Funerals = () => {
       });
   }, []);
 
+  const getPlaceById =() =>{
+    history.push(`places/type/${id}`);
+  }
   return (
     <>
-    <div className="places">
+    <div className="places" onClick={getPlaceById}>
       {showPlaces()}
       </div>
     </>
