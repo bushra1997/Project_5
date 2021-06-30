a45add41d149a5e1de3ba96ea55d925f2aafda8f
Noof <noof.mohammed998@gmail.com>
2021-06-30 11:20:00 +0300

    funerals style

--- frontend/src/components/Funerals/index.js
+++ frontend/src/components/Funerals/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
-import "./birthDayPart.css"
+import "./funerals.css"
 
 const Funerals = () => {
   const [places, setPlaces] = useState([]);
