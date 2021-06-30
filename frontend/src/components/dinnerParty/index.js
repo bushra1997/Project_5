910fdf127b1bd16caade851df9d10f93de1c3494
Noof <noof.mohammed998@gmail.com>
2021-06-30 17:37:09 +0300

    dinnerParty Component

--- frontend/src/components/dinnerParty/index.js
+++ frontend/src/components/dinnerParty/index.js
@@ -1,5 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory, useParams } from "react-router-dom";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import "./dinnerParty.css"
+import ShowPlaces from "../showPlaces";
+
 const DinnerParty = () => {
-  return <h4>DinnerParty</h4>;
+  const history = useHistory();
+  const [places, setPlaces] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/places/type/dinnerParty`)
+      .then((result) => {
+        if (result.status == 200) {
+          setPlaces(result.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="dinnerPartyPlace">
+        <ShowPlaces places={places} />
+      </div>
+    </>
+  );
 };
+
 export default DinnerParty;
