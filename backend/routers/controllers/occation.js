f12a56a2ce632da40646b476c5b2ce473395f9e9
Noof <noof.mohammed998@gmail.com>
2021-06-30 10:40:28 +0300

    createNewOcation

---
+++ backend/routers/controllers/occation.js
@@ -0,0 +1,14 @@
+const db = require("../../db/db");
+const CreateNewOccation = (req, res) => {
+  const { place_id, type } = req.body;
+
+  const query = `INSERT INTO place(place_id,type )VALUES(?,?)`;
+  const data = [place_id, type];
+  connection.query(query, data, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+};
+module.exports = {
+    CreateNewOccation,
+  };
--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,7 @@ app.use("/user", userRouter);
 app.use("/rating", ratingRouter);
 app.use("/login", loginRouter);
 app.use(placeRouter);
+
 //app routers
 
 const PORT = process.env.PORT || 5000;
