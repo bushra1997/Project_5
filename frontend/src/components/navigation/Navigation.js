4255250c029eaccb11f8222cb1c5ef045963d1c5
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-07-02 22:56:28 +0300

    trying to add styles on navBar

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -4,29 +4,47 @@ import NavBar from "./NavbarPage";
 
 import { Route, Link, useHistory } from "react-router-dom";
 
-
-
 const Navigation = () => {
   return (
     <>
-    <div className="Nav">
-      <div className="header"><Link exact to="/home">Ocassions App</Link></div>
-      <div className="header1"><NavBar/></div>
-    </div>
-    <div>
-      <Link to="/weddings">Weddings</Link>
-      <Link to="/birthDayParty">BirthDayParty </Link>
-      <Link to="/funerals">Funerals </Link>
-      <Link to="/dinnerParty">DinnerParty</Link>
-      <Link to="/meeting">Meeting</Link>
+      <div className="Nav">
+        <div className="header">
+          <Link className="logo-link" exact to="/home">
+            Ocassions App
+          </Link>
+        </div>
+        <div className="header1">
+          <NavBar />
+        </div>
       </div>
+        <div class="container">
+       <Link class="btn btn-1" to="/weddings"><svg>
+              <rect x="0" y="0" fill="none" width="100%" height="100%" />
+            </svg>Weddings</Link>
+      <Link class="btn btn-1"  to="/birthDayParty"><svg>
+              <rect x="0" y="0" fill="none" width="100%" height="100%" />
+            </svg>BirthDayParty</Link>
+      <Link class="btn btn-1"  to="/funerals"><svg>
+              <rect x="0" y="0" fill="none" width="100%" height="100%" />
+            </svg>Funerals </Link>
+      <Link class="btn btn-1"  to="/dinnerParty"><svg>
+              <rect x="0" y="0" fill="none" width="100%" height="100%" />
+            </svg>DinnerParty</Link>
+      <Link class="btn btn-1"  to="/meeting"><svg>
+              <rect x="0" y="0" fill="none" width="100%" height="100%" />
+            </svg>Meeting</Link>
+
+        
+        </div>
     </>
   );
 };
 
 export default Navigation;
 
-
-
-
- 
+// <div className="Nav">
+//   <div className="header"><Link className="logo-link" exact to="/home">Ocassions App</Link></div>
+//   <div className="header1"><NavBar/></div>
+// </div>
+// <div className="sections">
+//   </div>
