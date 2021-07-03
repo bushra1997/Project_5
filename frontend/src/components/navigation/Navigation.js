6a6a8709fc116df779b1e26daca29fa8bf405f2d
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-07-03 11:38:12 +0300

    working on css

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -21,27 +21,25 @@ const Navigation = () => {
        <Link class="btn btn-1" to="/weddings"><svg>
               <rect x="0" y="0" fill="none" width="100%" height="100%" />
             </svg>Weddings</Link>
-      <Link class="btn btn-1"  to="/birthDayParty"><svg>
+       <Link class="btn btn-1" to="/birthDayParty"><svg>
               <rect x="0" y="0" fill="none" width="100%" height="100%" />
-            </svg>BirthDayParty</Link>
+            </svg>BD Party</Link>
       <Link class="btn btn-1"  to="/funerals"><svg>
               <rect x="0" y="0" fill="none" width="100%" height="100%" />
             </svg>Funerals </Link>
       <Link class="btn btn-1"  to="/dinnerParty"><svg>
               <rect x="0" y="0" fill="none" width="100%" height="100%" />
-            </svg>DinnerParty</Link>
+            </svg>Dinner Party</Link>
       <Link class="btn btn-1"  to="/meeting"><svg>
               <rect x="0" y="0" fill="none" width="100%" height="100%" />
             </svg>Meeting</Link>
-
-        
         </div>
     </>
   );
 };
 
 export default Navigation;
-
+// birthDayParty
 // <div className="Nav">
 //   <div className="header"><Link className="logo-link" exact to="/home">Ocassions App</Link></div>
 //   <div className="header1"><NavBar/></div>
