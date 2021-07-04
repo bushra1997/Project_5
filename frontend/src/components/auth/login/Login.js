761d20804e27213f6194ea93e5e26266a418c00f
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-07-04 21:24:50 +0300

    link now redirect to registration

--- frontend/src/components/auth/login/Login.js
+++ frontend/src/components/auth/login/Login.js
@@ -80,7 +80,7 @@ export default function Login() {
                 }}
               />
             </div>
-            <Link className="signIn text-center">Dosn't have account?<br/>Sign up Now</Link>
+            <Link className="signIn text-center" exact to="/register">Dosn't have account?<br/>Sign up Now</Link>
           </form>
           <div className="btn-submit">
             <button
