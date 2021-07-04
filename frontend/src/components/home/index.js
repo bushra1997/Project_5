88c367ec64357675ad49e52a11402aa85cbf3d7c
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-07-04 22:29:43 +0300

    Structure Preif section

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -17,21 +17,29 @@ export default function Index() {
       .catch((err) => {});
   }, []);
   return (
-    <Carousel fade>
-      {images &&
-        images.map((e, i) => {
-          return (
-            <Carousel.Item>
-              <img className="d-block w-100" src={e.img} alt="First slide" />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          );
-        })}
-    </Carousel>
+    // <Carousel fade>
+    //   {images &&
+    //     images.map((e, i) => {
+    //       return (
+    //         <Carousel.Item>
+    //           <img className="d-block w-100" src={e.img} alt="First slide" />
+    //           <Carousel.Caption>
+    //             <h3>First slide label</h3>
+    //             <p>
+    //               Nulla vitae elit libero, a pharetra augue mollis interdum.
+    //             </p>
+    //           </Carousel.Caption>
+    //         </Carousel.Item>
+    //       );
+    //     })}
+    // </Carousel>
+    <>
+      <section>
+        <div className="container text-center">
+        <h1>Our Vision</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, voluptates officia culpa magnam recusandae accusamus. Ut, voluptas velit quasi, dolorum excepturi incidunt animi veritatis corporis corrupti adipisci dolore. Cumque, necessitatibus.</p>
+        </div>
+      </section>
+    </>
   );
 }
