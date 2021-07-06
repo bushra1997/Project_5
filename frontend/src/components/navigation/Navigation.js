c101b3f6929fb45fa60da8fc1c3959eb9767e247
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-07-06 15:41:13 +0300

    login only when not logged in

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./navigation.css";
-import { Route, Link, useHistory } from "react-router-dom";
-import Search from "./../search/search" ;
-import { useSelector } from "react-redux";
+import { Link} from "react-router-dom";
+import Search from "./../search/search";
+import { useDispatch, useSelector } from "react-redux";
+import { setOut } from "../../reducers/login/index";
 
 const Navigation = () => {
-  const [islogged, setIslogged] = useState(false)
+  const dispatch = useDispatch();
   const token = useSelector((state) => {
     return {
       token: state.token.token,
       user: state.token.user,
     };
   });
-  if (token) {
-    setIslogged(true)
-  } else {
-    setIslogged(false)
-  }
+  
+  const exist =token.token;
+    
+    
+  const signOut = () => {
+    dispatch(setOut({ token: "", user: {} }));
+    localStorage.setItem("token", "");
+    localStorage.setItem("role", {});
+  };
+  
+
+
   return (
     <>
       <nav class="navbar navbar-expand-lg navbar-white bg-white fixed-top">
@@ -34,83 +42,94 @@ const Navigation = () => {
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <Search/>
+        <Search />
         <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <Link exact to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Occasions
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <Link to="/weddings" className="dropdown-item">
+                Wedding
+              </Link>
+              <Link to="/birthDayParty" className="dropdown-item">
+                Birthday Party
+              </Link>
+              <Link to="/funerals" className="dropdown-item">
+                Funeral
+              </Link>
+              <Link to="/dinnerParty" className="dropdown-item">
+                Dinner Party
+              </Link>
+              <Link to="/meeting" className="dropdown-item">
+                Meeting
+              </Link>
+            </div>
+          </li>
+          <li class="nav-item">
+            <Link exact to="/" className="nav-link">
+              About us
+            </Link>
+          </li>
+        </ul>
+       {exist?( <li class="nav-item dropdown" style={{ listStyle: "none" }}>
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <span>
+              <img
+                className="style-scope yt-img-shadow"
+                src="https://static.wixstatic.com/media/065e6b_dd1a8624bd5b40c9848aeec671ed811b.png/v1/fit/w_936%2Ch_733%2Cal_c/file.png"
+                style={{ borderRadius: "50%", height: "32px", width: "32px" }} alt="Avatar image"
+              />
+            </span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li class="nav-item">
-              <Link exact to="/" className="nav-link">
-                Home
+              <Link onClick={signOut} exact to="/login" className="nav-link">
+                logout
               </Link>
             </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Occasions
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <Link to="/weddings" className="dropdown-item">
-                  Wedding
-                </Link>
-                <Link to="/birthDayParty" className="dropdown-item">
-                  Birthday Party
-                </Link>
-                <Link to="/funerals" className="dropdown-item">
-                  Funeral
-                </Link>
-                <Link to="/dinnerParty" className="dropdown-item">
-                  Dinner Party
-                </Link>
-                <Link to="/meeting" className="dropdown-item">
-                  Meeting
-                </Link>
-              </div>
-            </li>
+
             <li class="nav-item">
-              <Link exact to="/" className="nav-link">
-                About us
-              </Link>
-            </li>
-          </ul>
-        <li class="nav-item dropdown" style={{listStyle:"none"}}>
-
-<a
-  class="nav-link dropdown-toggle"
-  href="#"
-  id="navbarDropdown"
-  role="button"
-  data-toggle="dropdown"
-  aria-haspopup="true"
-  aria-expanded="false"
->
-<span> 
-            <img className="style-scope yt-img-shadow"  src="https://static.wixstatic.com/media/065e6b_dd1a8624bd5b40c9848aeec671ed811b.png/v1/fit/w_936%2Ch_733%2Cal_c/file.png" style={{borderRadius:"50%" ,height:"32px",width:"32px"}} />
-        </span>
-</a>
-<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-<li class="nav-item">
-              <Link exact to="/login" className="nav-link">
-                Login
-              </Link>
-            </li>
-
-<li class="nav-item">
               <Link exact to="/profile" className="nav-link">
                 My Profile
               </Link>
             </li>
-</div>
+            <li class="nav-item">
+              <Link exact to="/favorite" className="nav-link">
+                My favorite
+              </Link>
             </li>
+          </div>
+        </li>):( 
+        <li class="nav-item" style={{ listStyle: "none" }}>
+              <Link exact to="/login" className="nav-link">
+                Login
+              </Link>
+            </li>)}
       </nav>
     </>
   );
 };
 
 export default Navigation;
-
-
