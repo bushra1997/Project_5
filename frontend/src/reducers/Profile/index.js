e097239726fa88f192a5b697a7921e52b01812ef
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-07-06 20:29:14 +0300

    profile reducer added

---
+++ frontend/src/reducers/Profile/index.js
@@ -0,0 +1,23 @@
+const initialState = {
+    image: "",
+  };
+  
+  const profilePictuer = (state = initialState, { type, payload }) => {
+    switch (type) {
+      case "SET_IMAGE":
+        return { image: payload.image };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export const setImage = (image) => {
+    return {
+      type: "SET_IMAGE",
+      payload: image,
+    };
+  };
+  
+  export default image;
+  
