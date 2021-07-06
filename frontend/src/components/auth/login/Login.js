9dd7ccb5d047f9bbf6362432e4d3b980ef7383c8
Noof <noof.mohammed998@gmail.com>
2021-07-06 09:54:31 +0300

    styleLogin

--- frontend/src/components/auth/login/Login.js
+++ frontend/src/components/auth/login/Login.js
@@ -6,6 +6,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useDispatch, useSelector } from "react-redux";
 import "./login.css";
 import { Link } from "react-router-dom";
+import {
+  Form,
+  Button,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+} from "react-bootstrap";
 
 export default function Login() {
   const history = useHistory();
@@ -79,11 +86,14 @@ export default function Login() {
                 }}
               />
             </div>
+            
+            <div className="signIn">
             <Link className="signIn text-center" exact to="/register">
               Dosn't have account?
               <br />
               Sign up Now
             </Link>
+            </div>
           </form>
           <div className="btn-submit">
             <button
