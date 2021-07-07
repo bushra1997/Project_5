e8849171c41a92b8bf161cf19d8f865cd7b8f476
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-07-07 10:31:53 +0300

    update on showPlaces

--- frontend/src/components/showPlaces/index.js
+++ frontend/src/components/showPlaces/index.js
@@ -8,26 +8,35 @@ const ShowPlaces = ({ places }) => {
   const ShowCard = ({ place }) => {
     return (
       <>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4 col-md-6 col-sm-12">
-              <div className="card">
-                <div>
-                  <img src={place.img} class="card-img-top" alt="" />
-                </div>
-                <div className="card-body">
-                  <h5 class="card-title">{place.name}</h5>
-                  <p class="card-text">{place.description}</p>
-                  <p class="card-text">{place.capacity}</p>
-                  <p class="card-text">{place.city}</p>
-                  <p class="card-text">{place.address}</p>
-                  <p class="card-text">{place.availability}</p>
-                </div>
-              </div>
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <div className="card">
+            <div>
+              <img src={place.img} class="card-img-top" alt="" />
+            </div>
+            <div className="card-body">
+              <h5 class="card-title">{place.name}</h5>
+              <p class="card-text">{place.description}</p>
+              <p class="card-text">{place.capacity}</p>
+              <p class="card-text">{place.city}</p>
+              <p class="card-text">{place.address}</p>
+              <p class="card-text">{place.availability}</p>
             </div>
           </div>
         </div>
       </>
+      // <Card style={{ width: "18rem" }}>
+      //   <Card.Img variant="top" src={place.img} />
+      //   <Card.Body>
+      //     <Card.Title>{place.name}</Card.Title>
+      //     <Card.Text>{place.description}</Card.Text>
+      //   </Card.Body>
+      //   <ListGroup className="list-group-flush">
+      //     <ListGroupItem>capacity: {place.capacity}</ListGroupItem>
+      //     <ListGroupItem>city: {place.city}</ListGroupItem>
+      //     <ListGroupItem>address: {place.address}</ListGroupItem>
+      //     <ListGroupItem>availability: {place.availability}</ListGroupItem>
+      //   </ListGroup>
+      // </Card>
     );
   };
 
@@ -43,7 +52,9 @@ const ShowPlaces = ({ places }) => {
 
   return (
     <>
-      <div className="places">{showPlaces()}</div>
+      <div className="container">
+        <div className="row">{showPlaces()}</div>
+      </div>
     </>
   );
 };
