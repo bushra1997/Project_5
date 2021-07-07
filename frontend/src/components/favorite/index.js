ae027831bee1d6bcc8e1e85847f0a95fd0dfdb39
Noof <noof.mohammed998@gmail.com>
2021-07-07 18:34:40 +0300

    clean code

--- frontend/src/components/favorite/index.js
+++ frontend/src/components/favorite/index.js
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import "./favorite.css";
 
 export default function Favorite() {
-  const [favorateCard, setFavorateCard] = useState("")
+  const [favorateCard, setFavorateCard] = useState("");
   const token = useSelector((state) => {
     return {
       token: state.token.token,
@@ -13,52 +13,49 @@ export default function Favorite() {
     };
   });
   useEffect(() => {
-    axios.get(`http://localhost:5000/favorite/${token.user.id}`)
-    .then((result)=>{
-      setFavorateCard(result.data)
-    })
-    .catch((err)=>{
-      console.log(err.message);
-    })
-  }, [])
-  
-  console.log("aaaaaaaaaaaaa",favorateCard);
-  return (
-   
-    
-    <div className="favorite-container">
-    {favorateCard
-    &&favorateCard.map((place,i)=>{
-      return (
-        <>
-        {console.log(favorateCard,"oooo")}
-         <div className="container">
-         <div key={i} className="favorate-card"><img src={place.img} style={{width:"100%"}}></img></div>
-         <div className="card-body">
-         <h5 class="card-title">{place.name}</h5>
-         </div>
-         <ul class="list-group">
-              <li class="list-group-item card-text" className="description">
-                <i class="fas fa-comment-alt"></i>
-                {place.description}
-              </li>
-              <div className="address">
-                <li class="list-group-item card-text">
-                  <i class="fas fa-users"></i>
-                  {place.capacity}
-                </li>
-                <li class="list-group-item card-text">
-                  <i class="fas fa-map-pin"></i>
-                  {place.address}
-                </li>
-              </div>
-            </ul>
-         </div>
-        </>
-        )
-      })}
+    axios
+      .get(`http://localhost:5000/favorite/${token.user.id}`)
+      .then((result) => {
+        setFavorateCard(result.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
+  return (
+    <div className="favorite-container">
+      {favorateCard &&
+        favorateCard.map((place, i) => {
+          return (
+            <>
+              <div className="container">
+                <div key={i} className="favorate-card">
+                  <img src={place.img} style={{ width: "100%" }}></img>
+                </div>
+                <div className="card-body">
+                  <h5 class="card-title">{place.name}</h5>
+                </div>
+                <ul class="list-group">
+                  <li class="list-group-item card-text" className="description">
+                    <i class="fas fa-comment-alt"></i>
+                    {place.description}
+                  </li>
+                  <div className="address">
+                    <li class="list-group-item card-text">
+                      <i class="fas fa-users"></i>
+                      {place.capacity}
+                    </li>
+                    <li class="list-group-item card-text">
+                      <i class="fas fa-map-pin"></i>
+                      {place.address}
+                    </li>
+                  </div>
+                </ul>
+              </div>
+            </>
+          );
+        })}
     </div>
-    
   );
 }
