1dd98b1c4b3507dd0c837831658f2a35027775e2
Noof <noof.mohammed998@gmail.com>
2021-07-07 09:33:27 +0300

    clean code

--- frontend/src/components/showPlaces/index.js
+++ frontend/src/components/showPlaces/index.js
@@ -8,34 +8,35 @@ const ShowPlaces = ({ places }) => {
   const ShowCard = ({ place }) => {
     return (
       <div className="row">
-      <div className="showPlaces_" >
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={place.img} />
-        <div className="bodyShowPlaces">
-        <Card.Body>
-          <Card.Title>{place.name}</Card.Title>
-          <div className="description">
-          <Card.Text>{place.description}</Card.Text>
-          </div>
-        </Card.Body>
+        <div className="showPlaces_">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={place.img} />
+            <div className="bodyShowPlaces">
+              <Card.Body>
+                <Card.Title>{place.name}</Card.Title>
+                <div className="description">
+                  <Card.Text>{place.description}</Card.Text>
+                </div>
+              </Card.Body>
+            </div>
+            <div className="group">
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>capacity: {place.capacity}</ListGroupItem>
+                <ListGroupItem>city: {place.city}</ListGroupItem>
+                <ListGroupItem>address: {place.address}</ListGroupItem>
+                <ListGroupItem>
+                  availability: {place.availability}
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Card>
         </div>
-        <div className="group">
-        <ListGroup className="list-group-flush">
-          <ListGroupItem>capacity: {place.capacity}</ListGroupItem>
-          <ListGroupItem>city: {place.city}</ListGroupItem>
-          <ListGroupItem>address: {place.address}</ListGroupItem>
-          <ListGroupItem>availability: {place.availability}</ListGroupItem>
-        </ListGroup>
-        </div>
-      </Card>
-      </div>
       </div>
     );
   };
 
   const showPlaces = () => {
     return places.map((place) => (
-  
       <Link to={`/places/${place.type}/${place.place_id}`}>
         <div key={place.id} className="place-card">
           <ShowCard place={place} />
