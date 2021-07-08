a3f1c69d5b828835d45e4035b4afb7d8ae7e38be
Noof <noof.mohammed998@gmail.com>
2021-07-08 19:11:05 +0300

    about component update paragraph

--- frontend/src/components/about/index.js
+++ frontend/src/components/about/index.js
@@ -9,7 +9,7 @@ const About = () => {
     <div>
       <img src="https://www.arabiaweddings.com/sites/default/files/styles/400x400/public/companies/images/2017/09/al_hamra_palace_halls.jpg?itok=xcRw9e3N"></img>
     </div>
-    <div>Our website to provide ‘A Gateway for a ‘Happier for All’ through inclusive activities and services. Our site is distinguished by providing you with service and providing opportunities to get what you want from the luxury
+    <div>Our website distinguished by providing you with service and providing opportunities to get what you want from the luxury, to provide is through inclusive activities and services. 
 
       <p></p>
     </div>
