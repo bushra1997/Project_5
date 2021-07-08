7a6f245638b1823ff9b36c818867ce7c9aa7d7b6
Noof <noof.mohammed998@gmail.com>
2021-07-08 19:13:39 +0300

    about component update img

--- frontend/src/components/about/index.js
+++ frontend/src/components/about/index.js
@@ -7,7 +7,7 @@ import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 const About = () => {
   return <div className="container">
     <div>
-      <img src="https://www.arabiaweddings.com/sites/default/files/styles/400x400/public/companies/images/2017/09/al_hamra_palace_halls.jpg?itok=xcRw9e3N"></img>
+      <img src="https://www.arabiaweddings.com/sites/default/files/styles/max750/public/tips/2016/01/amman_outdoor_venues.jpg?itok=x8aybcyp"></img>
     </div>
     <div>Our website distinguished by providing you with service and providing opportunities to get what you want from the luxury, to provide is through inclusive activities and services. 
 
