ed52f63b3f4f430fec718d427000dc9510cfedb3
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-07-12 10:40:35 +0300

    update

--- frontend/src/components/booking/index.js
+++ frontend/src/components/booking/index.js
@@ -107,7 +107,7 @@ const Booking = () => {
                 <button
                   type="button"
                   class="btn btn-primary mb-2"
-                  style={{ margin: "0px 0px", backgroundColor:"#fff", color:"#cf6262" fon}}
+                  style={{ margin: "0px 0px", backgroundColor:"#fff", color:"#cf6262", fontWeight:"600"}}
                   onClick={Back}
                 >
                   Cancel
