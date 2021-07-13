f09f1d55128c407c870fda566e97fd74df9523db
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-07-13 10:45:16 +0300

    update carousel height and width

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -60,18 +60,7 @@ export default function Index() {
             />
           </div>
         </div>
-        <a
-          className="carousel-control-prev"
-          href="#carouselExampleFade"
-          role="button"
-          data-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Previous</span>
-        </a>
+        
         <a
           className="carousel-control-next"
           href="#carouselExampleFade"
