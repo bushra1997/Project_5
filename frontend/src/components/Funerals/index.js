6eb6f29ca963953ff2723db4ea52f5ea363a4388
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-07-16 19:27:47 +0300

    clean code

--- frontend/src/components/Funerals/index.js
+++ frontend/src/components/Funerals/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useHistory, useParams } from "react-router-dom";
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./funerals.css";
 import ShowPlaces from "../showPlaces/index";
 
